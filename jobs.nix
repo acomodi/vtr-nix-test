f3cb1367e8c426aca744be01d156823c60d25ac6
Dustin DeWeese <dustin.deweese@gmail.com>
2020-05-07 14:59:28 -0700

    add vtr_reg_strong to jobs.nix

--- jobs.nix
+++ jobs.nix
@@ -2,5 +2,6 @@ with import ./default.nix {};
 
 {
   vtr_reg_basic = regression_tests.vtr_reg_basic.all;
+  vtr_reg_strong = regression_tests.vtr_reg_strong.all;
   baseline_inner_num_sweep = baseline_inner_num_sweep.all;
 }

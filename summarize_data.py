b6bfa5e92b55f04b72c249771302098d13ad38ce
Dusty DeWeese <dustin.deweese@gmail.com>
2020-05-09 18:56:05 -0700

    add data.feather as a build product

--- summarize_data.py
+++ summarize_data.py
@@ -1,15 +1,16 @@
 import os
+from os.path import join
 import sys
 import re
 import pandas as pd
 
-summary_file_name = "data.feather"
-results_file_name = "parse_results.txt"
+summary_file_name = 'data.feather'
+results_file_name = 'parse_results.txt'
 
 def process_data(root):
-  df = pd.read_table(os.path.join(root, results_file_name), sep=' *\t *', engine='python')
-  params = read_params(root, "params")
-  vtr_opts = read_params(root, "vtr_opts")
+  df = pd.read_table(join(root, results_file_name), sep=' *\t *', engine='python')
+  params = read_params(root, 'params')
+  vtr_opts = read_params(root, 'vtr_opts')
   df['vtr_variant'] = vtr_opts['variant']
   df['task'] = params['task']
   df['run_id'] = params['run_id']
@@ -22,9 +23,9 @@ def process_data(root):
 
 def read_params(root, fn):
   data = {}
-  with open(os.path.join(root, fn)) as f:
+  with open(join(root, fn)) as f:
     for l in f.readlines():
-      l = l.split(":", 1)
+      l = l.split(':', 1)
       if(len(l) < 2): continue
       data[l[0]] = l[1].strip()
   return data
@@ -34,8 +35,8 @@ os.mkdir(out_dir)
 dfs = []
 for root in sys.argv[1:]:
   files = os.listdir(root)
-  if "summary" in files:
-    summary_path = os.path.join(root, "summary", summary_file_name)
+  if 'summary' in files:
+    summary_path = join(root, 'summary', summary_file_name)
     if os.path.exists(summary_path):
       dfs += [pd.read_feather(summary_path)]
   else:
@@ -44,4 +45,9 @@ for root in sys.argv[1:]:
 if len(dfs) > 0:
   df = pd.concat(dfs, ignore_index=True, sort=False)
   df = df.replace(-1, pd.NA) # -1 is treated as a missing value
-  df.to_feather(os.path.join(out_dir, summary_file_name))
+  df.to_feather(join(out_dir, summary_file_name))
+
+nix_support = join(out_dir, 'nix-support')
+os.mkdir(nix_support)
+with open(join(nix_support, 'hydra-build-products'), 'w') as f:
+  f.write('file feather {}\n'.format(summary_file_name))

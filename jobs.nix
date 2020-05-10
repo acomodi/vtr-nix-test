fcacd699a53fb7eb92001369d07345abcb1b0593
Dusty DeWeese <dustin.deweese@gmail.com>
2020-05-09 18:59:17 -0700

    .all -> .summary

--- jobs.nix
+++ jobs.nix
@@ -1,7 +1,7 @@
 with import ./default.nix {};
 
 {
-  vtr_reg_basic = regression_tests.vtr_reg_basic.all;
-  vtr_reg_strong = regression_tests.vtr_reg_strong.all;
-  baseline_inner_num_sweep = baseline_inner_num_sweep.all;
+  vtr_reg_basic = regression_tests.vtr_reg_basic.summary;
+  vtr_reg_strong = regression_tests.vtr_reg_strong.summary;
+  baseline_inner_num_sweep = baseline_inner_num_sweep.summary;
 }

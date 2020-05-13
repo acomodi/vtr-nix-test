b3e64bdf8af7c520be02c0c2426fbf8da80c5c4b
Dusty DeWeese <dustin.deweese@gmail.com>
2020-05-12 17:46:24 -0700

    add job with just directed moves test

---
+++ jobs/directed_moves.nix
@@ -0,0 +1,8 @@
+{ ... }: # ignore arguments
+
+with import ../default.nix { pkgs = import <nixpkgs> {}; }; # import default.nix, passing in nixpkgs
+
+# each attribute is a job
+{
+  directed_moves_sweep = directed_moves_sweep.summary;
+}

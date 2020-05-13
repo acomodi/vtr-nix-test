ad6550862391e43596e80cd1b7d7f157243433b6
Dusty DeWeese <dustin.deweese@gmail.com>
2020-05-12 17:20:32 -0700

    fix jobs.nix

--- jobs.nix
+++ jobs.nix
@@ -1,7 +1,8 @@
-{ nixpkgs ? import <nixpkgs> {}, ... }:
+{ ... }: # ignore arguments
 
-with import ./default.nix { pkgs = nixpkgs; };
+with import ./default.nix { pkgs = import <nixpkgs> {}; }; # import default.nix, passing in nixpkgs
 
+# each attribute is a job
 {
   vtr_reg_basic = regression_tests.vtr_reg_basic.summary;
   vtr_reg_strong = regression_tests.vtr_reg_strong.summary;

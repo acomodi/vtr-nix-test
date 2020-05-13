ab77493e2d48ad1c9b8eb76ac475d22be3f68548
Dusty DeWeese <dustin.deweese@gmail.com>
2020-05-13 05:00:23 -0700

    add more regression tests

--- jobs/regression_tests.nix
+++ jobs/regression_tests.nix
@@ -6,4 +6,6 @@ with import ../default.nix { pkgs = import <nixpkgs> {}; }; # import default.nix
 {
   vtr_reg_basic = regression_tests.vtr_reg_basic.summary;
   vtr_reg_strong = regression_tests.vtr_reg_strong.summary;
+  vtr_reg_nightly = regression_tests.vtr_reg_nightly.summary;
+  vtr_reg_weekly = regression_tests.vtr_reg_weekly.summary;
 }

b7ca876c33c075871bf85313cd53dc84cf184994
Dusty DeWeese <dustin.deweese@gmail.com>
2020-07-20 13:56:37 -0700

    add find_seed.nix

---
+++ jobs/find_seed.nix
@@ -0,0 +1,29 @@
+{
+  pkgs ? import <nixpkgs> {}
+}:
+
+with import ../library.nix {
+  inherit pkgs;
+  default_vtr_rev = "0a6ce4d388ecee8aa6564307df71c3a5c26107ec";
+};
+
+with pkgs.lib;
+
+let
+  vtr_dusty_sa = vtrDerivation {
+    variant = "dusty_sa";
+    url = "https://github.com/HackerFoo/vtr-verilog-to-routing.git";
+    ref = "dusty_sa";
+    rev = "c12ab323885b6fdbe3d55adf086a59f2ce04587d";
+  };
+in
+listToAttrs (map (seed: {
+  name = "seed_${toString seed}";
+  value = (make_regression_tests {
+    vtr = vtr_dusty_sa;
+    flags = { inherit seed; };
+  }).vtr_reg_nightly.vtr_bidir.summary;
+}) (range 128 256))
+
+    
+      

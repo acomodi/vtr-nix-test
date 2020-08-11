43440fb5b8222972f3e5cf401db16a801a61be6f
Dusty DeWeese <dustin.deweese@gmail.com>
2020-08-11 13:20:45 -0700

    update jobs/node_reordering.nix

--- jobs/node_reordering.nix
+++ jobs/node_reordering.nix
@@ -2,29 +2,27 @@
 
 with import ../library.nix {
   pkgs = import <nixpkgs> {};
-  default_vtr_rev = "06847757816efed89e5216bfaf15c118498bedc4";
+  default_vtr_rev = "d4ea40548ee30aa13e27df5268760a86e38ad289";
 }; # import default.nix, passing in nixpkgs
 
 let vtr_node_reordering = vtrDerivation {
       variant = "node_reordering";
       url = "https://github.com/HackerFoo/vtr-verilog-to-routing.git";
       ref = "node_reordering_flag";
-      rev = "fb381c011f3b83deb1c63275ee0b923ea9c8151c";
+      rev = "70cbb48d50458999dee0566b286182e8ed387f60";
     };
 in
 summariesOf {
-  base = make_regression_tests {};
+  base = (make_regression_tests {}).vtr_reg_nightly.titan_quick_qor;
   node_reordering =
     let test = {flags, ...}:
           (make_regression_tests {
             vtr = vtr_node_reordering;
-            flags = flags // {
-              reorder_rr_graph_nodes_threshold = 1;
-            };
-          }).all;
+            inherit flags;
+          }).vtr_reg_nightly.titan_quick_qor;
     in
       flag_sweep "node_reordering" test {
-        reorder_rr_graph_nodes_algorithm = ["degree_bfs" "random_shuffle"];
+        reorder_rr_graph_nodes_algorithm = ["none" "degree_bfs" "random_shuffle"];
       };
 }
 

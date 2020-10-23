5c25b69575a090cae667ed9add37babee247e7c0
Alessandro Comodi <acomodi@antmicro.com>
2020-10-23 15:16:31 +0200

    Add base cost job
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

---
+++ jobs/robust_base_cost.nix
@@ -0,0 +1,36 @@
+{
+  pkgs ? import <nixpkgs> {}
+}:
+
+with import ../library.nix {
+  inherit pkgs;
+  default_vtr_rev = "461f8539372b36492d88e58a7b9675ebfe703760";
+};
+
+with pkgs.lib;
+
+let
+  vtr_base_cost = vtrDerivation {
+    variant = "extended_lookahead";
+    url = "https://github.com/acomodi/vtr-verilog-to-routing.git";
+    ref = "robust-delay-norm-factor";
+    rev = "f2634acba01206382f388d7cabfbbcefbccf7ac6";
+  };
+
+  vtr_default = vtrDerivation {
+    variant = "baseline";
+    url = "https://github.com/acomodi/vtr-verilog-to-routing.git";
+    ref = "robust-delay-norm-factor";
+    rev = "ab5f508db7e405925a45d1da918e6dca78730b44";
+  };
+
+in
+summariesOf {
+  base_regression_tests = (make_regression_tests {
+    vtr = vtr_default;
+  }).vtr_reg_nightly.titan_quick_qor;
+
+  changes_regression_tests = (make_regression_tests {
+    vtr = vtr_base_cost;
+  }).vtr_reg_nightly.titan_quick_qor;
+}

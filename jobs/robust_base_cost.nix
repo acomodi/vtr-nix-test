3b4285f3258321c8fc355dd28d5ceccc382c75ca
Alessandro Comodi <acomodi@antmicro.com>
2020-11-06 17:07:40 +0100

    base cost histogram
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- jobs/robust_base_cost.nix
+++ jobs/robust_base_cost.nix
@@ -10,18 +10,18 @@ with import ../library.nix {
 with pkgs.lib;
 
 let
-  vtr_base_cost = vtrDerivation {
+  vtr_base_cost_histogram = vtrDerivation {
     variant = "robust_base_cost";
     url = "https://github.com/acomodi/vtr-verilog-to-routing.git";
-    ref = "robust-delay-norm-factor";
-    rev = "5e2b7eab3809f946ed57b648a4187986096cfdf9";
+    ref = "robust-base-cost-mode";
+    rev = "769828c1363f1343f86afc0aa8b04a776035f44d";
   };
 
   vtr_default = vtrDerivation {
     variant = "baseline";
     url = "https://github.com/acomodi/vtr-verilog-to-routing.git";
-    ref = "robust-delay-norm-factor";
-    rev = "ab5f508db7e405925a45d1da918e6dca78730b44";
+    ref = "robust-base-cost-mode";
+    rev = "236bfcd720e163ab3853d8500fa9cfa222a3240c";
   };
 
 in
@@ -31,6 +31,6 @@ summariesOf {
   }).vtr_reg_nightly.titan_quick_qor;
 
   changes_regression_tests = (make_regression_tests {
-    vtr = vtr_base_cost;
+    vtr = vtr_base_cost_histogram;
   }).vtr_reg_nightly.titan_quick_qor;
 }

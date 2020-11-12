b5313c82ea33ef65c201b0de8e4d3d3bff044f98
Alessandro Comodi <acomodi@antmicro.com>
2020-11-12 20:30:00 +0100

    update nightly job
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- jobs/robust_base_cost.nix
+++ jobs/robust_base_cost.nix
@@ -14,14 +14,21 @@ let
     variant = "robust_base_cost";
     url = "https://github.com/acomodi/vtr-verilog-to-routing.git";
     ref = "robust-delay-norm-factor";
-    rev = "78e218cbba0fae8771f0cb130bec27122e7a7bf0";
+    rev = "5c0393eb7280803ce3a8b29ad2bafa3fff841e3e";
+  };
+
+  vtr_base_cost_histogram_higher_delay_astar = vtrDerivation {
+    variant = "robust_base_cost";
+    url = "https://github.com/acomodi/vtr-verilog-to-routing.git";
+    ref = "robust-delay-norm-factor";
+    rev = "1a239e6a9eae5bdbe783192bbc1278bd1027b614";
   };
 
   vtr_default = vtrDerivation {
     variant = "baseline";
     url = "https://github.com/acomodi/vtr-verilog-to-routing.git";
     ref = "robust-delay-norm-factor";
-    rev = "2efa8d0bff34688a02b4fec7e0b5b7d44824f6c1";
+    rev = "00a7efdeb49aafd61d2eed7834bccc6110f3160d";
   };
 
 in
@@ -32,5 +39,9 @@ summariesOf {
 
   changes_regression_tests = (make_regression_tests {
     vtr = vtr_base_cost_histogram;
+  }).vtr_reg_nightly.titan_quick_qor;
+
+  changes_regression_tests_higher_astar = (make_regression_tests {
+    vtr = vtr_base_cost_histogram_higher_delay_astar;
   }).vtr_reg_nightly.titan_quick_qor;
 }

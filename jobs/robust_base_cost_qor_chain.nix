5240a28c85725937c6c3d02f18020a1696d0775c
Alessandro Comodi <acomodi@antmicro.com>
2020-11-13 10:10:57 +0100

    add double pres fac
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- jobs/robust_base_cost_qor_chain.nix
+++ jobs/robust_base_cost_qor_chain.nix
@@ -13,21 +13,28 @@ let
   vtr_base_cost_histogram = vtrDerivation {
     variant = "robust_base_cost";
     url = "https://github.com/acomodi/vtr-verilog-to-routing.git";
-    ref = "robust-delay-norm-factor";
+    ref = "robust-delay-norm-factor-double-pres-fac";
     rev = "057650382e99d5938833a8de04496c3c9660f5e2";
   };
 
   vtr_base_cost_histogram_higher_delay_astar = vtrDerivation {
     variant = "robust_base_cost";
     url = "https://github.com/acomodi/vtr-verilog-to-routing.git";
-    ref = "robust-delay-norm-factor";
+    ref = "robust-delay-norm-factor-double-pres-fac";
     rev = "22084174e52bbe2c4f548bda47db9e07aafa5ba4";
+  };
+
+  vtr_double_pres_fac = vtrDerivation {
+    variant = "baseline";
+    url = "https://github.com/acomodi/vtr-verilog-to-routing.git";
+    ref = "robust-delay-norm-factor-double-pres-fac";
+    rev = "363e99f51e7daefd5031ad8e4a974afe96e2db13";
   };
 
   vtr_default = vtrDerivation {
     variant = "baseline";
     url = "https://github.com/acomodi/vtr-verilog-to-routing.git";
-    ref = "robust-delay-norm-factor";
+    ref = "robust-delay-norm-factor-double-pres-fac";
     rev = "00a7efdeb49aafd61d2eed7834bccc6110f3160d";
   };
 
@@ -43,5 +50,9 @@ summariesOf {
 
   changes_regression_tests_higher_astar = (make_regression_tests {
     vtr = vtr_base_cost_histogram_higher_delay_astar;
+  }).vtr_reg_nightly.vtr_reg_qor_chain;
+
+  changes_regression_tests_higher_astar_double_pres_fac = (make_regression_tests {
+    vtr = vtr_double_pres_fac;
   }).vtr_reg_nightly.vtr_reg_qor_chain;
 }

67df0d89990c12547815ea649e94da063fc2b417
Alessandro Comodi <acomodi@antmicro.com>
2020-11-13 10:01:58 +0100

    update versions
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- jobs/robust_base_cost.nix
+++ jobs/robust_base_cost.nix
@@ -14,14 +14,14 @@ let
     variant = "robust_base_cost";
     url = "https://github.com/acomodi/vtr-verilog-to-routing.git";
     ref = "robust-delay-norm-factor";
-    rev = "5c0393eb7280803ce3a8b29ad2bafa3fff841e3e";
+    rev = "057650382e99d5938833a8de04496c3c9660f5e2";
   };
 
   vtr_base_cost_histogram_higher_delay_astar = vtrDerivation {
     variant = "robust_base_cost";
     url = "https://github.com/acomodi/vtr-verilog-to-routing.git";
     ref = "robust-delay-norm-factor";
-    rev = "1a239e6a9eae5bdbe783192bbc1278bd1027b614";
+    rev = "22084174e52bbe2c4f548bda47db9e07aafa5ba4";
   };
 
   vtr_default = vtrDerivation {
